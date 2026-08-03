4f6ab2f495e543d6a4e8317d5ad3a0376eda818e
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-03 16:34:33 -0400

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        for i,val in enumerate(operations):
+            if val == "C": 
+                operations.pop(i)
+                operations.pop(i-1)
+                i -= 2
+        print(operations)
+        ret = 0 
+        if len(operations) ==0 : return 0
+        if len(operations) ==1 : return int(operations[0])
+        prev1, prev2 = operations[0], operations[1]
+
+        for i,val in enumerate(operations):
+            if val == "+":
+                ret += prev1 + prev2
+                prev1, prev2 = prev2, prev1 + prev2
+            elif val == "D":
+                ret += prev2 * 2
+                prev1 = prev2
+                prev2 = prev2 * 2
+            else:
+                ret += int(val)
+                prev1 = prev2
+                prev2 = int(val)
+            
+                
+            
+        return ret

6277a4afb232b53919b12a481a3aac345a0e034c
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-05 18:44:31 -0400

    Add: delete-node-in-a-bst - submission-3

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-3.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        
+        def findNode(node,key):
+            if node == None:
+                return
+
+            if node.left != None and node.left.val == key:
+                return node
+            
+            if node.right != None and node.left.val == key:
+                return node
+    
+            if key < node.val:
+                return findNode(node.left,key)
+            else:
+                return findNode(node.right,key)
+        
+        parent = findNode(root,key)
+
+        deleteNode = parent.left if parent.left.val == key else parent.right
+
+        replacement = deleteNode.right
+        smallerNodes = deleteNode.left
+
+        smallest = replacement
+        while smallest.left:
+            smallest = smallest.left
+        
+        smallest.left =smallerNodes
+
+        if parent.left.val == key :
+            parent.left = replacement
+        else:
+            parent.right = replacement
+        
+        return root
+
+
+
+            

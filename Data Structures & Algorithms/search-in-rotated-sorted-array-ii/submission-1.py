18b38e0ef01fadd0874ca6fd9092f3f6960f94f0
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-06 15:23:57 -0400

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        l=nums[0]
+        rotate = 0
+        for i,val in enumerate(nums):
+            if val < l:
+                rotate = i
+                break
+            l=val
+        
+        l,r = rotate, (rotate-1)+len(nums)
+        # 0 1 2 3 4 5 6 7| 8 9 10 11 12
+        # 3 4 4 5 6 1 2 2| 3 4 4  5  6
+        # 1 2 2 3 4 4 5 6
+
+        #0,1,2,3,4,5,6
+        #3,5,6,0,0,1,2
+        #0,0,1,2,3,5,6
+
+
+        #mapped r index  = r%len(nums)
+        #mapped mid = (r+l)/2%len(nums)
+        mid = None
+        while l<=r :
+            print(l)
+            print(r)
+            mid = (r+l)//2
+            print(mid)
+            if target < nums[mid%len(nums)]:
+                r = mid-1
+            elif target > nums[mid%len(nums)]:
+                l = mid+1
+            else:
+                return True
+        
+        return False
+
+
+            

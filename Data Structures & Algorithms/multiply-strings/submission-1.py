f80bc4e7c6edc88eac1ae3901d5838a9ca621f1c
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-13 15:34:27 -0400

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        multiplier = 1
+        firstNum = 0
+        for i in range(len(num1)-1,-1,-1):
+            firstNum += (ord(num1[i]) - ord('0')) * multiplier
+            multiplier *= 10
+        
+        multiplier = 1
+        secondNum = 0
+        for i in range(len(num2)-1,-1,-1):
+            secondNum += (ord(num2[i]) - ord('0')) * multiplier
+            multiplier *= 10
+        
+        print(firstNum)
+        print(secondNum)
+        return str(firstNum * secondNum)

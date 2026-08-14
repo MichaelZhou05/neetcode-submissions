18674debdca4c4b09250e3f94aa7657d79789eb9
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-13 20:24:22 -0400

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ret = [0,1,1,2,1] 
+        for i in range(5,n+1):
+                ret.append(1+ret[i-4])
+        
+        return ret if n>=4 else ret[:n+1]

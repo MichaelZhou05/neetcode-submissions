d500b4ac21d4c63832868ea7f0c28becd584a48d
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-16 13:58:26 -0400

    Add: excel-sheet-column-title - submission-2

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        # 3--> * 10 + 1
+
+        # A = 1--> * 26
+        # AZ = 1*26 + 26  = 2*26 =  52
+        # BA = 2*26 + 1
+        ret = []
+        letters = [chr(ord('@') + i) for i in range(26)]
+        letters[0] = 'Z'
+        while columnNumber:
+            ret.append(letters[columnNumber%26])
+            if columnNumber > 26:
+                columnNumber = columnNumber //26
+            else: 
+                columnNumber = 0
+        ret.reverse()
+        return "".join(ret)

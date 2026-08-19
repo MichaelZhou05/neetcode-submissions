1e2d1f507f3533e75e86737d18d79d8fb160bb5c
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-18 21:32:08 -0400

    Add: jump-game-vii - submission-0

---
+++ Data Structures & Algorithms/jump-game-vii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        dp=[False for _ in range(len(s))]
+
+        dp[0] = True
+        #[T,F,F,F,T,F,F,T]
+        #[0,0,1,1,0,0,1,0]
+        #           ^   ^ 
+        #count = 3
+        window = maxJump - minJump
+        i = window
+        count=0
+        while i<len(s):
+            if dp[i-window]:
+                count = window+1
+            while i<len(s) and count:
+                if s[i] == '0':
+                    dp[i] = True
+                if dp[i-window]:
+                    count = window+1
+
+                i += 1
+                count -= 1
+
+            i += 1
+        print(dp)
+        return dp[-1]
+        

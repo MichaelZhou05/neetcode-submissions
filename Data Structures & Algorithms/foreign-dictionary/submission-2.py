80a390fb60ebbfa846e1b60b53e1c452552380cc
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-18 20:16:56 -0400

    Add: foreign-dictionary - submission-2

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        #keep track of all seen chars
+        #build adj list
+        # if cycle return "" (#edges >= #nodes)
+        # if abc before ab return ""
+        
+        seenChars = set(words[0])
+
+        adjList = [[] for _ in range(26)]
+        indegree = [0]*26
+
+        for i in range(1,len(words)):
+            seenChars.update(words[i])
+            lastWord = words[i-1]
+            currWord = words[i]
+            for j,char in enumerate(lastWord):
+                if j >= len(currWord): return ""        # if abc before ab return ""
+                if char != currWord[j]:
+                    adjList[ord(char) - ord('a')].append(currWord[j])
+                    indegree[ord(currWord[j]) - ord('a')] += 1
+                    break
+        que = []
+        for i,degree in enumerate(indegree):
+            if degree == 0 and chr(ord('a')+i) in seenChars:
+               que.append(i)
+        # print(indegree)
+        # print(que)
+
+        ret = []
+        while que:
+            nextQue = []
+            for i in que:
+                ret.append(chr(ord('a')+i))
+                for char in adjList[i]:
+                    indegree[ord(char)-ord('a')] -= 1
+                    if indegree[ord(char)-ord('a')] == 0:
+                        nextQue.append(ord(char)-ord('a'))
+
+            que = nextQue
+
+        return "".join(ret) if len(ret) == len(seenChars) else ""
+
+
+
+

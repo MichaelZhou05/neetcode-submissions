2b7c371525928d2b7fa5db2c7570dffd1c1101d1
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-21 21:42:26 -0400

    Add: split-array-largest-sum - submission-1

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        l,r = max(nums), sum(nums)
+        minSum = r
+        def canMake(maxSubVal,k):
+            currSum = 0
+            for val in nums:
+                currSum += val
+                if currSum > maxSubVal:
+                    k-=1
+                    currSum = val
+            return k > 0
+
+        while l<=r:
+            mid = (l+r)//2
+            if canMake(mid,k):
+                print("canMake" + str(mid))
+                minSum = mid
+                r = mid -1
+            else:
+                l = mid + 1
+        
+        return minSum

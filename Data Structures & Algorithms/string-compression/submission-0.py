6f9c81828799094c7d7b626b8a43f9b9bd1bfc56
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-23 18:48:53 -0400

    Add: string-compression - submission-0

---
+++ Data Structures & Algorithms/string-compression/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        startingChar = chars[0]
+        i = 0 #read index
+        repeats = 0
+
+        j = 0       #write index
+
+        while i<len(chars):
+            print(startingChar,repeats)
+            if chars[i] == startingChar:
+                i += 1
+                repeats += 1
+            else:                           #char[i] is different
+                if repeats > 1:
+                    compressed = startingChar + str(repeats)
+                    for k in range(len(compressed)):
+                        chars[j] = compressed[k]
+                        j+=1
+                        print(j)
+                else:
+                    chars[j] = startingChar
+                    j+= 1
+                
+                startingChar = chars[i]
+                repeats = 1
+                i += 1
+
+        
+        if repeats > 1:
+            compressed = startingChar + str(repeats)
+            for k in range(len(compressed)):
+                chars[j] = compressed[k]
+                j+=1
+                print(j)
+        else:
+            chars[j] = startingChar
+            j+= 1
+
+        return j
+
+            

20211449f50e35aee5cf2c3f308ef47047a7436c
MichaelZhou05 <135999648+MichaelZhou05@users.noreply.github.com>
2026-08-24 14:15:25 -0400

    Add: triangle - submission-2

---
+++ Data Structures & Algorithms/triangle/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0 for _ in range(n)] for _ in range(n)] # dp[r][i] = min path to bot of triangle
+
+
+        for r in range(n-1,-1,-1):
+            for i in range(r+1):
+                if r+1 < n:
+                    dp[r][i] += min(dp[r+1][i], dp[r+1][i+1])
+                dp[r][i] += triangle[r][i]
+
+        return dp[0][0]
